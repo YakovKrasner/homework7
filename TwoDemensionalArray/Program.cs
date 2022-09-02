7d0f72a516c252aec43906994424557051e438ee
Yakov Krasner <yakov.krasner@yandex.ru>
2022-09-02 20:42:50 +0700

    Initial commit_RandomTwoDemensionArray

---
+++ TwoDemensionalArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int FirstDimension = 5; // число строк
+int SecondDimension = 6; // число столбцов
+double [,] Array = new double [FirstDimension,SecondDimension];
+for (int i = 0; i < Array.GetLength (0); i ++)
+    {    
+         for (int j = 0; j < Array.GetLength (1); j ++)
+            {
+                Array [i,j] = new Random().NextDouble();
+                Console.Write ($" {Array[i,j]}");
+            }
+        Console.WriteLine ();
+    }
+
